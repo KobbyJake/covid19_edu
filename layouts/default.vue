8c33881d7fa66e5dffe61a2f155293a8c4f6ff6a
KobbyJake <appiahbaahjake@gmail.com>
2020-05-21 15:05:08 +0100

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-app dark>
+    <!-- <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+    <v-app-bar elevate-on-scroll fixed app color="light-blue darken-2">
+      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" /> -->
+      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? "right" : "left"}` }}</v-icon>
+      </v-btn> -->
+      <!-- <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>mdi-application</v-icon>
+      </v-btn> -->
+      <!-- <v-btn icon @click.stop="fixed = !fixed">
+        <v-icon>mdi-minus</v-icon>
+      </v-btn> -->
+
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <!-- <v-btn text class="text-none">
+        <v-icon>mdi-file-edit</v-icon>
+        Register
+      </v-btn> -->
+      <v-btn text class="text-none">
+        <v-icon>mdi-lock-open</v-icon>
+        login
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <nuxt />
+    </v-content>
+    <!-- <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+    <v-footer :fixed="fixed" app color="light-blue darken-2">
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // clipped: false,
+      // drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      // miniVariant: false,
+      // right: true,
+      // rightDrawer: false,
+      title: 'Covid-19 Educator'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,191 @@
+<template>
+  <div>
+    <v-container mt-8>
+      <br />
+      <v-row d-flex reverse wrap>
+        <v-col class="animated fadeInLeftBig">
+          <v-container text-center align-center justify-center class="hero">
+            <img src="~assets/medicare.svg" alt="shsc logo" width="400" />
+          </v-container>
+        </v-col>
+        <v-col>
+          <p class="font-weight-black display-3 animated fadeInRightBig">
+            COVID-19 Educator.
+          </p>
+          <hr />
+          <br />
+          <p
+            class="font-weight-normal headline animated bounceInRight delay-1000"
+          >
+            Educating Ghanaian basic school students on the safety precautions
+            to prevent COVID-19 infection and also manage infection.
+          </p>
+          <br />
+
+          <div mt-5>
+            <v-dialog v-model="dialog" persistent max-width="600px">
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  outlined
+                  rounded
+                  color="light-blue lighten-2"
+                  class="text-none"
+                  v-on="on"
+                  ><v-icon left>mdi-file-edit</v-icon>Register</v-btn
+                >
+                <!-- <v-btn color="primary" dark v-on="on">Open Dialog</v-btn> -->
+              </template>
+              <v-card>
+                <v-card-title>
+                  <v-icon left large>mdi-file-edit</v-icon>
+                  <span class="headline text-center">Student Registration</span>
+                </v-card-title>
+                <hr />
+                <v-card-text>
+                  <v-form ref="form" v-model="valid">
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="6">
+                          <v-text-field
+                            label="Legal first name*"
+                            required
+                            :rules="firstNameRules"
+                          ></v-text-field>
+                        </v-col>
+
+                        <v-col cols="12" sm="6" md="6">
+                          <v-text-field
+                            label="Legal last name*"
+                            required
+                            :rules="lastNameRules"
+                          ></v-text-field>
+                        </v-col>
+
+                        <v-col cols="12" sm="6">
+                          <!-- <v-select
+                          :items="['0-17', '18-29', '30-54', '54+']"
+                          label="Age*"
+                          required
+                        ></v-select> -->
+                          <v-text-field
+                            required
+                            type="number"
+                            max="25"
+                            min="4"
+                            label="age"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                          <v-select
+                            :items="['Male', 'Female']"
+                            label="Gender*"
+                          ></v-select>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field label="Name of school*"></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="6">
+                          <v-autocomplete
+                            :items="[
+                              'Oti',
+                              'Bono East',
+                              'Ahafo',
+                              'North East',
+                              'Savannah',
+                              'Western North',
+                              'Western',
+                              'Volta',
+                              'Greater Accra',
+                              'Ashanti',
+                              'Central',
+                              'Upper East',
+                              'Upper West'
+                            ]"
+                            label="Region of school*"
+                            required
+                          ></v-autocomplete>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="6">
+                          <v-text-field
+                            label="Class of study*"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-form>
+                  <small>*indicates required field</small>
+                </v-card-text>
+                <hr />
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    outlined
+                    rounded
+                    color="default"
+                    class="text-none"
+                    @click="dialog = false"
+                    >Close</v-btn
+                  >
+                  <v-btn
+                    outlined
+                    rounded
+                    color="light-blue lighten-2"
+                    class="text-none"
+                    @click="dialog = false"
+                    >Save</v-btn
+                  >
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  data() {
+    return {
+      dialog: true,
+      valid: false,
+      name: '',
+      lastNameRules: [
+        (v) => !!v || 'Last name is required',
+        (v) => (v && v.length <= 2) || 'Name must be less than a character'
+      ],
+      firstNameRules: [
+        (v) => !!v || 'First name is required',
+        (v) => (v && v.length <= 2) || 'Name must be less than a character'
+      ],
+
+      email: '',
+      emailRules: [
+        (v) => !!v || 'E-mail is required',
+        (v) => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+      ],
+      select: null,
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4'],
+      checkbox: false,
+      lazy: false
+    }
+  }
+  // components: {
+  //   Logo,
+  //   VuetifyLogo
+  // }
+}
+</script>
+<style>
+.banner {
+  height: 100%;
+}
+.hero {
+  transform: translateY(5%);
+}
+</style>
