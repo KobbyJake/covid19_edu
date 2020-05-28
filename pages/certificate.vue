5c4eee4cfd5ab4dd50cdedb595df27821fa62a82
KobbyJake <appiahbaahjake@gmail.com>
2020-05-29 00:03:19 +0100

    improved certificate

--- pages/certificate.vue
+++ pages/certificate.vue
@@ -1,24 +1,32 @@
 <template>
-  <v-row class="fill-height" align="center" justify="center">
+  <v-row class="fill-height wall" align="center" justify="center">
     <v-card class="mx-auto" max-width="500">
       <v-card-title primary-title>
-        <p class="display-1 text--primary">
+        <p class="display-1 text--primary text-center">
           Certificate of Participation
         </p>
       </v-card-title>
       <v-card-text>
-        <div>Word of the Day</div>
-        <p class="display-1 text--primary">
-          This is to certify that
+        <hr />
+        <p class="display-2 text--primary text-center">
+          Recipient Name
         </p>
-        <p>Student</p>
-        <div class="text--primary">
-          Has completed the study of content on COVID-19
+        <div class="text--primary text-center">
+          Has completed the study of content on
         </div>
+        <p class="display-1 text--primary text-center">
+          COVID-19
+        </p>
+        <div class="text--primary text-left">On this day {{ new Date() }}</div>
+        <br />
+        <div class="text--primary text-right">
+          ...Signed...
+        </div>
+        <hr />
       </v-card-text>
       <v-card-actions>
-        <v-btn text color="deep-purple accent-4" @click="print()">
-          Print Certificate
+        <v-btn outlined large class="text-none" @click="print">
+          Print
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -26,7 +34,18 @@
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    print() {
+      window.print()
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.wall {
+  background: url(~assets/cool-background.png) no-repeat center fixed;
+  background-size: cover;
+}
+</style>
